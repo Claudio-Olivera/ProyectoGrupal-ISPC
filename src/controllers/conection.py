3b214024544e2a657697fcaeafaced3335a4b21e
Claudio-Olivera <claudioolivera753@gmail.com>
2022-10-29 22:06:41 -0300

    creacion de tablas, move controllers & models

--- src/controllers/conection.py
+++ src/controllers/conection.py
@@ -1,7 +1,7 @@
 import mysql.connector
 from mysql.connector import Error
 try:
-	connection=mysql.connector.connect(host='localhost',database='new_schema',user='root',password='rionegri12')
+	connection=mysql.connector.connect(host='localhost',database='new_schema',user='root',password='TupasswordAca')
 	if connection.is_connected():
 			db_Info=connection.get_server_info()
 			print("Connected to MySQL Server version", db_Info)
