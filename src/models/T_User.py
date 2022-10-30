d96f59ebdf2d500936ea816b8f122ffa0214fc68
Claudio-Olivera <claudioolivera753@gmail.com>
2022-10-29 21:27:42 -0300

    a

---
+++ src/models/T_User.py
@@ -0,0 +1,29 @@
+import mysql.connector
+
+try:
+    connection = mysql.connector.connect(host='localhost',
+                                        database='new_schema',
+                                        user='root',
+                                        password='rionegri12')
+
+
+#Claudio Olivera: -El password de la tabla user esta como varchar, pero se puede pasar a binary y guardarla despues de sufrir una encriptacion, como por ejemplo con sha-256
+
+    mySql_Create_Table_Query = """CREATE TABLE User ( 
+                            Id_User int(11) NOT NULL AUTO_INCREMENT,
+                            User varchar(20) NOT NULL UNIQUE,
+                            Email varchar(250) NOT NULL UNIQUE,
+                            Password varchar(30) NOT NULL,
+                            PRIMARY KEY (Id_User)) """
+
+    cursor = connection.cursor()
+    result = cursor.execute(mySql_Create_Table_Query)
+    print("tabla de usuario creada con exito!! ")
+
+except mysql.connector.Error as error:
+    print("Fallo al crear tabla en MySQL: {}".format(error))
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("Conexion con MySQL cerrada.")
---
+++ src/models/T_more_Data.py
@@ -0,0 +1,36 @@
+import mysql.connector
+
+try:
+    connection = mysql.connector.connect(host='localhost',
+                                        database='new_schema',
+                                        user='root',
+                                        password='rionegri12')
+
+
+#Claudio Olivera: -El password de la tabla user esta como varchar, pero se puede pasar a binary y guardarla despues de sufrir una encriptacion, como por ejemplo con sha-256
+
+    mySql_Create_Table_Query = """CREATE TABLE more_Data ( 
+                            Id_more_Data int(11) NOT NULL,
+                            Name varchar(20) NOT NULL,
+                            LastName varchar(50) NOT NULL,
+                            Calle varchar(30) NOT NULL,
+                            Direccion varchar(30) NOT NULL,
+                            Piso_Depto varchar(30),
+                            Ciudad varchar(30) NOT NULL,
+                            Provincia varchar(30) NOT NULL,
+                            Id_user int(11) NOT NULL,
+                            CONSTRAINT FK_moreData_TO_User FOREIGN KEY (Id_user) REFERENCES User(Id_User)
+                                ON UPDATE RESTRICT ON DELETE CASCADE,
+                            PRIMARY KEY (Id_more_Data)) """
+
+    cursor = connection.cursor()
+    result = cursor.execute(mySql_Create_Table_Query)
+    print("tabla de more_Data creada con exito!! ")
+
+except mysql.connector.Error as error:
+    print("Fallo al crear tabla en MySQL: {}".format(error))
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("Conexion con MySQL cerrada.")
