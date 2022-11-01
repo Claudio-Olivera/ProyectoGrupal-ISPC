3554d54f84fb7255ee3feb96a9407709d8b82d19
Claudio-Olivera <claudioolivera753@gmail.com>
2022-10-31 23:11:50 -0300

    Inserts, deletes, selects, updates..

--- src/controllers/controllersAnteriores/notificadorDeMail.py
+++
@@ -1,7 +0,0 @@
-'''
-Esto deberia ser un servicio que este en ejecucion permanentemente, analizando 
-si corresponde enviar algun mail por diferentes motivos, por ejemplo:
-* Mail solicitando puntuaciones
-* Mail recordando proximidad de turnos.
-
-'''
